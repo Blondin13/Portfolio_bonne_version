24c7fcbb9bd57384246992c91506a15583cb8c94
Blondin13 <buirechristophe@gmail.com>
2022-02-23 17:15:33 +0100

    Projet portfolio version final

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ let transporter = nodemailer.createTransport({
 
 const app = express(); // Créer l'application express
 app.use(express.static(resolve(__dirname, "public")));
-app.set("views", __dirname + "/public");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(8000, () => {
     console.log("Server a démarer dans http://localhost:8000");
 });
 
 app.post("/contact", async (req, res) => {
+    let message = "";
     let mailOptions = {
         from: req.body.email,
         to: "buirechristophe@gmail.com",
@@ -28,6 +28,30 @@ app.post("/contact", async (req, res) => {
         text: req.body.message,
     };
 
-    await transporter.sendMail(mailOptions);
-    res.redirect("/index.html");
+    transporter.sendMail(mailOptions, (err) => {
+        if (err) {
+            message = "Votre message n'est pas transmis !";
+            res.render("contact.html.twig", { message });
+        } else {
+            message = "Votre message est transmis !";
+            res.render("contact.html.twig", { message });
+        }
+    });
+    
+});
+
+app.get("/contact", async (req, res) => {
+    res.render("contact.html.twig");
+});
+
+app.get("/", async (req, res) => {
+    res.render("index.html.twig");
+});
+
+app.get("/cv", async (req, res) => {
+    res.render("cv.html.twig");
+});
+
+app.get("/projets", async (req, res) => {
+    res.render("projets.html.twig");
 });
